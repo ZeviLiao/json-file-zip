9f73edb515718dd3319f7d9424a79631dd8def5c
Zevi Liao <jointjm@gmail.com>
2020-09-17 15:43:40 +0800

    dir recursive

--- src/index.js
+++ src/index.js
@@ -5,14 +5,25 @@ var JSZip = require("jszip");
 var fs = require("fs");
 var path = require("path");
 var zip = new JSZip();
-var file_content = fs.readFileSync(path.join(__dirname,'./archive/presentation-48.json'));
-zip.file("presentation-48.json", file_content);
+
+const jsonFolder = path.join(__dirname, './archive/');
+
+fs.readdir(jsonFolder, (err, files) => {
+    files.forEach(file => {
+        const filePath = jsonFolder + file
+        var file_content = fs.readFileSync(filePath);
+        zip.file(file, file_content);
+    });
+});
+
+
 // zip.file("Hello.txt", "Hello World\n");
 // var img = zip.folder("images");
 // img.file("smile.gif", imgData, {base64: true});
-zip.generateAsync({type:"nodebuffer"})
-.then(function(content) {
-    fs.writeFile("presentation-48.zip", content, function (err) {
-        if (err) throw err;
+zip.generateAsync({ type: "nodebuffer" })
+    .then(function (content) {
+        fs.writeFile(`presentation-${+ new Date()}.zip`, content, function (err) {
+            if (err) throw err;
+            console.log('done')
+        });
     });
-});
