828fd34f18f1b833f9fe5e234aa30f35a278dccd
Zevi Liao <jointjm@gmail.com>
2020-09-17 15:20:39 +0800

    zip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('hello')
+
+
+
+var JSZip = require("jszip");
+var fs = require("fs");
+var path = require("path");
+var zip = new JSZip();
+var file_content = fs.readFileSync(path.join(__dirname,'./archive/presentation-48.json'));
+zip.file("presentation-48.json", file_content);
+// zip.file("Hello.txt", "Hello World\n");
+// var img = zip.folder("images");
+// img.file("smile.gif", imgData, {base64: true});
+zip.generateAsync({type:"nodebuffer"})
+.then(function(content) {
+    fs.writeFile("presentation-48.zip", content, function (err) {
+        if (err) throw err;
+    });
+});
